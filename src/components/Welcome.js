472be000ca317945da7038fac865fedd8734672d
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-08-27 23:35:50 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,7 @@
 import React from 'react'
 const Welcome = (prop)=>{
   return(
-    <div>
-        <h1>Hey !</h1>
+    <div>       
         <h1>{prop.name}</h1>
         <h2>Welcome to Newton School.</h2>
     </div>
